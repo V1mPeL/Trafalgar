378ddc42b5ea8807a3ffb32ff55841d6feef611d
Sprynskyi Vitaliy <53211007+V1mPeL@users.noreply.github.com>
2022-06-05 16:30:47 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 
-// ========NAVBAR MENU TOGGLER===========
-let menuBtn = document.querySelector('#menu-btn');
-let navBar = document.querySelector('.navbar-menu');
-let closeBtn = document.querySelector('#navbar-menu-close');
-
-
-menuBtn.onclick = ()=>{
-    navBar.classList.toggle('navbar-menu-active');
-    navBar.classList.remove('navbar-menu');
-}
-
-closeBtn.onclick = ()=>{
-    navBar.classList.remove('navbar-menu-active');
-    navBar.classList.toggle('navbar-menu');
-}
-
-
+    // ========NAVBAR MENU TOGGLER===========
+    let menuBtn = document.querySelector('#menu-btn');
+    let navBar = document.querySelector('.navbar-menu');
+    let closeBtn = document.querySelector('#navbar-menu-close');
+    
+    
+    menuBtn.onclick = ()=>{
+        navBar.classList.toggle('navbar-menu-active');
+        navBar.classList.remove('navbar-menu');
+    }
+    
+    closeBtn.onclick = ()=>{
+        navBar.classList.remove('navbar-menu-active');
+        navBar.classList.toggle('navbar-menu');
+    }
+    
